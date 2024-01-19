a3edf96dbcaa732be768e23cb45ad974ef1be0e3
Sahil <sahil.dixit15.sd@gmail.com>
2024-01-19 17:06:59 +0530

    server files added

---
+++ index.js
@@ -0,0 +1,53 @@
+import express, { json } from "express";
+import axios from "axios";
+// All Imports
+import OpenAI from "openai";
+import * as dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+// const express = require('express');
+const app = express();
+
+app.use(cors());
+// app.use(express.static("public"));
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("listning at port 5000........");
+});
+const openai = new OpenAI({
+  apiKey: "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",
+});
+
+// api
+// Regestration Api's
+
+const runFun = async (request, res) => {
+  console.log("runFun");
+  // console.log(request);
+  console.log(request.body);
+  const reqBody= JSON.parse(request.body.body);
+  let prompt = reqBody.prompt;
+  console.log(prompt);
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "user", content: prompt },
+    ],
+    temperature: 1,
+    max_tokens: 64,
+    n: 1,
+    top_p: 1,
+    presence_penalty: 0,
+    frequency_penalty: 0,
+  });
+  //   console.log(response.data.choices);
+  console.log(response.choices[0].message);
+  const result = response.choices[0].message.content;
+  console.log(result);
+  res.send(result);
+};
+
+app.post("/api/chatgpt", runFun);
